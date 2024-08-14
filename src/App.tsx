879276caf87b569e7ae2b00d2591c540c29826d1
joseph941230revature <joseph941230@hotmail.com>
2024-08-14 12:03:13 -0700

    front_login

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import axios from 'axios';
+import type { FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import logo from './assets/Revconnect.png';
 
-function App() {
-  const [count, setCount] = useState(0)
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// clean user infomation
+onLoginPage();
+function onLoginPage(){
+  localStorage.removeItem("userInfo");
 }
 
-export default App
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+
+  axios.post("http://192.168.68.78:8080/home/login", values).then((response) => {
+    console.log(response.status, response.data);
+       //login success
+       let userInfo = response.data;
+       console.log("userInfo.username",userInfo.username)
+       localStorage.setItem("userInfo",JSON.stringify(userInfo));
+       alert("login success,welcome "+userInfo.username)
+       
+       // transfer tab to Revature
+       window.location.href="https://revature.com/"
+  }).catch(e => {
+      //login faild 
+      alert("Incorrect username or password, Please try again!")  
+  });
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+
+};
+
+const App: React.FC = () => (
+
+<div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <img
+        src={logo} 
+        alt="Logo"
+        style={{
+          width: '238px',  
+          height: 'auto',   
+          borderRadius: '0px',  
+        }}
+      />
+    
+
+
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input placeholder = "Enter your email or Username"/>
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{ offset: 8, span: 16 }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  </div>
+);
+
+
+export default App;
