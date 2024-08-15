930515f8779f6136be70ab95603a6a86acfb2f4d
Phone Myat <hanminsoe10@gmail.com>
2024-08-15 15:16:59 -0500

    delete a file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import{ BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 
 function App() {
 
