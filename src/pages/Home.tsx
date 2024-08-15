ccf9186d9a3fb384c2d542e2ec45a54d2a82af8c
Matt <themlp@icloud.com>
2024-08-15 17:40:07 -0400

    mattp - use proper jsx elements in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,10 @@
 const HomePage = () => {
   return (
-    <div>
-      {" "}
-      <div class="container py-6 mx-auto">
-        <h1>Hello, Bootstrap and Vite!</h1>
-        <button class="btn btn-primary text-bg-danger p-3">
-          Primary button
-        </button>
-      </div>{" "}
+    <div className="container py-6 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button className="btn btn-primary text-bg-danger p-3">
+        Primary button
+      </button>
     </div>
   );
 };
