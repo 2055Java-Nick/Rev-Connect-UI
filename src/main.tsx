382f5fa5bf0ccb5bcc0cc3010c50bbd16647bfc9
Matt <themlp@icloud.com>
2024-08-15 16:55:50 -0400

    mattp/trevorl - add  bootstrap to the project

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.ts";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const HomePage: React.FC = () => {
-  return <div> HELLO WORLD </div>;
+const HomePage = () => {
+  return (
+    <div>
+      {" "}
+      <div class="container py-6 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button class="btn btn-primary text-bg-danger p-3">
+          Primary button
+        </button>
+      </div>{" "}
+    </div>
+  );
 };
 
 export default HomePage;
