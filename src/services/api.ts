13c32111784080d36fe0cb1bb7badd25fd5650f0
Matt <themlp@icloud.com>
2024-08-15 13:55:01 -0400

    mattp - setup service layer and apiContext for the application

---
+++ src/services/api.ts
@@ -0,0 +1,12 @@
+// base for making api calls
+// API class object
+import axios from "axios";
+
+const apiContext = axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiContext;
