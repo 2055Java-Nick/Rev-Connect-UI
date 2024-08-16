960af19c41b591fc010eeca0db0657d35fb1f49e
cs-cbonh002 <bonham_36@hotmail.com>
2024-08-16 10:33:58 -0400

    Addition of logo to register screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Register from './Registration/Register'
+import RevconnectLogo from './assets/Revconnect.png'
 
 const App: React.FC = () => {
   return (
     <div>
+      <img src={RevconnectLogo} alt="RevConnect Logo" width={700} height={300}/>
       <h1>Welcome to RevConnect!</h1>
       <h1>Register Here!</h1>
       <Register />
