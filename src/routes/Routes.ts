2eba5248daf57f8fee37ef9b3fd9e0c0c8a9ee9f
Matt <themlp@icloud.com>
2024-08-16 10:09:59 -0400

    mattp-trevorl-mv files to better represent frontend structure

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../pages/Home";
+import HomePage from "../pages/Home/Home";
 import App from "../App";
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
