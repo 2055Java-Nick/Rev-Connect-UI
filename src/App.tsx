6670d81adb8eecf51817c8cc1f51dc2e4538860d
rmrahman430 <rayyanmrahman@gmail.com>
2024-08-18 16:57:33 -0500

    repo cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CommentsSection from './components/CommentsSection'
+import { useState } from "react";
+import "./App.css";
+import CommentsSection from "./components/CommentsSection";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,9 +26,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <CommentsSection userId={1} postId={1}/>
+      <CommentsSection userId={1} postId={1} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
