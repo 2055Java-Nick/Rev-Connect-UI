96c206d526dcf1f2de9e48971bf366b635c715ee
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2024-08-19 13:09:53 -0400

    edited display based on login user, added logo and some styling

--- src/components/BusinessProfile/BusinessProfile.tsx
+++ src/components/BusinessProfile/BusinessProfile.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import React from 'react'
 import axios from 'axios';
+import { useUser } from '../Context/UserContext';
 
 interface BusinessProfileProps{   
 
@@ -17,8 +18,14 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
     const [firstName, setFirstName] = useState<string>('');
     const [lastName, setLastName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
-    const [business, setBusiness] = useState(false);
-    const [profileUserId, setProfileUserId] = useState('');
+    const [business, setBusiness] = useState<boolean>(false);
+    const [profileUserId, setProfileUserId] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+    const { user, setUser } = useUser();
+ 
+    useEffect(() => {
+        // console.log('User context:', user);
+    }, [user]);
 
 
     useEffect(() => {
@@ -32,21 +39,13 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
                 setEmail(response.data.EMAIL)
                 setBusiness(response.data.IS_BUSINESS)
                 setProfileUserId(response.data.USER_ID)
+                setError(null);
             })
             .catch((error) => {
                 console.error(error);
+                setError("No Profile For This User");
             });
     }, []);
-
-    // temporary AuthUser Object
-    const tempUser = { 
-      id: 1234,
-      username: "TestDummy1234",
-      firstName: "John",
-      lastName: "Doe",
-      email: "testdummy1234@email.notreal",
-      isBusiness: true
-    } 
 
     const path_url = `http://localhost:8080/profiles/business/${id}`;
 
@@ -76,44 +75,55 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
     };
 
   return (
-    <div className='BusinessProfile'>
-        <h1 className='BusinessProfile_title'>
-          { username }
-        </h1>
-        <p>{ `${ firstName } ${ lastName }` }</p>
-        <p>{ email }</p>
-        
+    <div>
+        <img src="/src/assets/Revconnect.png" alt='RevConnect Logo' className='w-25 mx-3 my-3'/>
         {
-            business ? <h2>&lt; Links /&gt; Component (to be added)</h2> : null
-        }
-
-        { isEditing ? (
-
-        <form onSubmit={ handleSubmit }>
-          <textarea
-            value={ bioFormData }
-            onChange={ handleChange }
-            placeholder="Tell Everyone About Yourself!"
-          />
-          <div>
-            <button type="submit">Submit</button>
-            <button type="button" onClick={ toggleEdit }>
-                Cancel
-            </button>
-          </div>
-        </form>
-      ) : (
-        <div>
-          <h2 className="BusinessProfile_bio">
-            { bioText ? bioText : "Tell Everyone About Yourself!" }
-          </h2>
+          error ? (
+            <p>{error}</p>
+          ) : (
+        <div className='d-flex flex-column justify-content-center align-items-center text-center'>
+            <h1 className='text-center'>
+            { username }
+            </h1>
+            <div className="border rounded px-5 py-1">
+              <p className="text-center">{ `${ firstName } ${ lastName }` }</p>
+              <p className="text-center">{ email }</p>
+            </div>
           {
-            // If authUser id = profile.user_id, option to edit bio
-            // User.id === profileUserId ? <button onClick={toggleEdit}>Edit</button> : null
-            <button onClick={toggleEdit}>Edit</button>
+              business ? <h2>&lt; Links /&gt; Component (to be added)</h2> : null
           }
-        </div>
-      )}
+  
+          { isEditing ? (
+          <form onSubmit={ handleSubmit } className="text-center">
+            <textarea
+              value={ bioFormData }
+              onChange={ handleChange }
+              placeholder="Tell Everyone About Yourself!"
+            />
+            <div className="text-center">
+              <button type="submit" disabled={ bioFormData.length > 500 ? true : false}>Submit</button>
+              <button type="button" onClick={ toggleEdit }>
+                  Cancel
+              </button>
+            </div>
+          </form>
+          ) : (
+          <div>
+            <div className="text-center border rounded px-5 py-2">
+              <h3 className='rounded px-5 py-2'>About:</h3>
+              <p className="BusinessProfile_bio">
+                { bioText ? bioText : "Tell Everyone About Yourself!" }
+              </p>
+            </div>
+              {
+                // user  && user.id == profileUserId ? <button onClick={toggleEdit}>Edit</button> : null
+                <button onClick={toggleEdit} className='my-2'>Edit</button>
+              }
+            </div>
+        )}
+        </div> 
+        )
+      }
     </div>
   )
 }
