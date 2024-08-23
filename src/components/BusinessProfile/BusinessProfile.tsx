d523f381998ecd8639a895c6ceab760990971aa0
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2024-08-22 22:34:54 -0400

    edited tests and added documentation

--- src/components/BusinessProfile/BusinessProfile.tsx
+++ src/components/BusinessProfile/BusinessProfile.tsx
@@ -26,24 +26,25 @@ interface EndorsementLink {
 }
 
 const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
-    let { id } = useParams();
+  // destructing the id that is included in the url path
+  let { id } = useParams();
 
-    const [bioText, setBioText] = useState<string>('');
-    const [isEditing, setIsEditing] = useState<boolean>(false);
-    const [bioFormData, setBioFormData] = useState<string>('');
-    const [success, setSuccess] = useState<string>('');
-    const [username, setUsername] = useState<string>('');
-    const [firstName, setFirstName] = useState<string>('');
-    const [lastName, setLastName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [business, setBusiness] = useState<boolean>(false);
-    const [profileUserId, setProfileUserId] = useState<number | null>(null);
-    const [error, setError] = useState<string | null>(null);
+    const [bioText, setBioText] = useState<string>('');                               // Api profile entity bio
+    const [isEditing, setIsEditing] = useState<boolean>(false);                       // state to toggle editing bio
+    const [bioFormData, setBioFormData] = useState<string>('');                       // form input state for updating bio
+    const [success, setSuccess] = useState<string>('');                               // success message for updating bio
+    const [username, setUsername] = useState<string>('');                             // Api profile entity
+    const [firstName, setFirstName] = useState<string>('');                           // Api profile entity first name of profile user
+    const [lastName, setLastName] = useState<string>('');                             // Api profile entity last name of profile user
+    const [email, setEmail] = useState<string>('');                                   // Api profile entity email address
+    const [business, setBusiness] = useState<boolean>(false);                         // Api profile entity value of whether this is a business profile or not
+    const [profileUserId, setProfileUserId] = useState<number | null>(null);          // Api profile entity User_id
+    const [error, setError] = useState<string | null>(null);                          // error depending on whether a profile exists for a user
     const [endorsementLinks, setEndorsementLinks] = useState<EndorsementLink[]>([]);
-    const { user, setUser } = useUser();
+    const { user, setUser } = useUser();                                              // Logged in user from UserContext
 
     useEffect(() => {
-        // console.log('User context:', user);
+        //console.log('User context:', user);
     }, [user]);
 
     useEffect(() => {
@@ -58,11 +59,10 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
       }
     }, [business, profileUserId]); 
 
-
+    // Setting states from Api profile object, dependency is path parameter id
     useEffect(() => {
         axios.get(path_url)
             .then((response) => {
-                console.log(response);
                 setBioText(response.data.BIO_TEXT);
                 setBioFormData(response.data.BIO_TEXT);
                 setUsername(response.data.USERNAME);
@@ -77,18 +77,22 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
                 console.error(error);
                 setError("No Profile For This User");
             });
-    }, []);
+    }, [id]);
 
+    // path variable for business profile calls
     const path_url = `http://localhost:8080/profile/business/${id}`;
 
+    // toggles display/access to the update bio textarea field
     const toggleEdit = () => {
         setIsEditing(!isEditing);
     };
 
+    // handles textarea value change
     const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setBioFormData(event.target.value);
     };
 
+    // handles submitting a new bio update
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         axios.patch(path_url, {
@@ -108,10 +112,12 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
 
     const handleAddOrUpdateLink = async (endorsementLink: EndorsementLink) => {
       try {
-        let updatedLink: EndorsementLink[];
+        let updatedLink: EndorsementLink;
         if (endorsementLink.id) {
           updatedLink = await updateEndorsementLinkService(endorsementLink);
-          setEndorsementLinks((endorsementLinks) => ((endorsementLink.id === updatedLink?.id)) ? updatedLink : endorsementLinks);
+          setEndorsementLinks(
+            endorsementLinks.map(endorsementLink => endorsementLink.id === updatedLink.id ? updatedLink : endorsementLink)
+          );
         } else {
           updatedLink = await createEndorsementLinkService(endorsementLink);
           setEndorsementLinks((endorsementLinks) => [...endorsementLinks, updatedLink]);
@@ -154,16 +160,18 @@ const BusinessProfile: React.FC<BusinessProfileProps> = ({ }) => {
                         <h2>Manage Your Endorsement Links</h2>
                         <EndorsementLinkForm userId={Number(profileUserId)} onSubmit={handleAddOrUpdateLink} />
                         <div className="py-3">
-                            {endorsementLinks.map((link) => (
-                                <div key={link.id} className="py-2">
-                                    <a href={link.link} target="_blank" rel="noopener noreferrer">
-                                        {link.linkText}
-                                    </a>
-                                    <button onClick={() => handleDeleteLink(link.id!)} className="mx-3">
-                                      Delete
-                                    </button>
-                                </div>
-                            ))}
+                            {endorsementLinks && endorsementLinks.map && endorsementLinks.map(link => {
+                              return(
+                              <div key={link.id} className="py-2">
+                                <a href={link.link} target="_blank" rel="noopener noreferrer">
+                                    {link.linkText}
+                                </a>
+                                <button onClick={() => handleDeleteLink(link.id!)} className="mx-3">
+                                  Delete
+                                </button>
+                              </div>)
+                              }) 
+                            }
                         </div>
                     </div>
                 ) : null
