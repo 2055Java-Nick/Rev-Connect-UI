9a61194fa49ab3ca380c8c6034028174b7e62841
Matt <themlp@icloud.com>
2024-09-02 16:27:13 -0400

    mattp - #35 useMemo example

--- src/contexts/PostsContext.tsx
+++ src/contexts/PostsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useMemo, useState } from "react";
 import { useFetchPosts } from "../hooks/useFetchPosts";
 import { Post, PostUpdate } from "../types/postTypes";
 import { useCreatePost } from "../hooks/useCreatePost";
@@ -17,46 +17,56 @@ export function PostsProvider({ children }: { children: ReactNode }) {
   const { handleUpdatePost } = useUpdatePost();
   const { handleDeletePost } = useDeletePost();
 
-  function previousPage(): void {
-    if (page != undefined && page > 0) setPage(page - 1);
-  }
-  function nextPage(): void {
-    if (page != undefined) setPage(page + 1);
-  }
+  const value = useMemo(() => {
+    function previousPage(): void {
+      if (page != undefined && page > 0) setPage(page - 1);
+    }
+    function nextPage(): void {
+      if (page != undefined) setPage(page + 1);
+    }
 
-  async function createPost(post: PostUpdate): Promise<Post> {
-    const response = await handleCreatePost(post);
-    refetchPosts();
-    return response;
-  }
+    async function createPost(post: PostUpdate): Promise<Post> {
+      const response = await handleCreatePost(post);
+      refetchPosts();
+      return response;
+    }
 
-  async function updatePost(post: PostUpdate): Promise<Post> {
-    const response = await handleUpdatePost(post);
-    refetchPosts();
-    return response;
-  }
+    async function updatePost(post: PostUpdate): Promise<Post> {
+      const response = await handleUpdatePost(post);
+      refetchPosts();
+      return response;
+    }
 
-  async function deletePost(postId: number): Promise<void> {
-    await handleDeletePost(postId);
-    refetchPosts();
-  }
+    async function deletePost(postId: number): Promise<void> {
+      await handleDeletePost(postId);
+      refetchPosts();
+    }
+
+    return {
+      posts,
+      loading,
+      error,
+      createPost,
+      updatePost,
+      deletePost,
+      refetchPosts,
+      page,
+      goToPreviousPage: previousPage,
+      goToNextPage: nextPage,
+    };
+  }, [
+    posts,
+    loading,
+    error,
+    page,
+    setPage,
+    refetchPosts,
+    handleDeletePost,
+    handleUpdatePost,
+    handleCreatePost,
+  ]);
 
   return (
-    <PostsContext.Provider
-      value={{
-        posts,
-        loading,
-        error,
-        createPost,
-        updatePost,
-        deletePost,
-        refetchPosts,
-        page,
-        goToPreviousPage: previousPage,
-        goToNextPage: nextPage,
-      }}
-    >
-      {children}
-    </PostsContext.Provider>
+    <PostsContext.Provider value={value}>{children}</PostsContext.Provider>
   );
 }
